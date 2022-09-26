fab5cbab9fae51483c670fd355a6109db0b0b5c7
Hannah Choi <hannahchoi1031@gmail.com>
2022-09-26 10:06:10 +0900

    Update viewport-size-checker.js

--- viewport-size-checker.js
+++ viewport-size-checker.js
@@ -15,7 +15,7 @@ javascript: (function() {
     }
 
     container = document.createElement("div");
-    container.style.cssText = "position: fixed; padding: 1px 14px; top: 18px; right: 20px; text-align: center; border-radius: 7px; background-color: rgb(255, 106, 69); color: #fff; font-family: 'Barlow', 'sans-serif'; font-size: 18px; line-height: 28px; height: 30px; z-index: 10000; font-weight: normal;";
+    container.style.cssText = "position: fixed; padding: 1px 14px; top: 18px; right: 20px; text-align: center; border-radius: 7px; background-color: rgba(255, 106, 69, 0.8); color: #fff; font-family: 'Barlow', 'sans-serif'; font-size: 18px; line-height: 28px; height: 30px; z-index: 10000; font-weight: normal;";
     container.innerHTML = "<span>" + window.innerWidth + " X " + window.innerHeight + "</span>";
     document.body.appendChild(container);
     window.onresize = viewportSize;
