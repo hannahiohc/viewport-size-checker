d54aab0f3aa8224909f39b8881df5dce8e3a09fc
Hannah Choi <haena_choi@apple.com>
2024-04-16 10:51:53 +0900

    update

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # viewport-size-checker
-A bookmark tool to check your viewport size. <br>
-Available in Safari, Chrome, Firefox and maybe more.
+Viewport Size Checker is a Javascript bookmarklet tool to check your viewport size.<br>
+Available in Safari, Chrome, Firefox and more.
 
 ## How to use
-1. Add a bookmark in your browser.
-2. Copy JS code and paste to the URL of your new bookmark.
-3. Click the bookmark to get the viewport size.
-4. Check the top right corner of the browser.
+1. Copy and paste the following code to your bookmarks bar.
+```
+javascript:!function()%7Bvar%20e=document.createElement(%22link%22);e.setAttribute(%22rel%22,%22stylesheet%22),e.setAttribute(%22type%22,%22text/css%22),e.setAttribute(%22href%22,%22https://fonts.googleapis.com/css2?family=Barlow:wght@500&display=swap%22),document.head.appendChild(e);let%20n=%22viewport-size-container%22;function%20i()%7Bvar%20e=window.innerWidth+%22%20X%20%22+window.innerHeight,t=document.getElementsByClassName(n);0%3Ct.length&&(t%5B0%5D.innerHTML=%22%3Cspan%3E%22+e+%22%3C/span%3E%22)%7D%7B0%3C(e=document.getElementsByClassName(n)).length&&e%5B0%5D.parentNode.removeChild(e%5B0%5D);let%20t=document.createElement(%22div%22);t.className=n,t.style.cssText=%22position:%20fixed;%20padding:%201px%2014px;%20top:%2018px;%20right:%2020px;%20text-align:%20center;%20border-radius:%207px;%20background-color:%20rgba(255,%20106,%2069,%200.8);%20color:%20#fff;%20font-family:%20'Barlow',%20'sans-serif';%20font-size:%2018px;%20line-height:%2028px;%20height:%2030px;%20z-index:%2010000;%20font-weight:%20normal;%22,document.body.appendChild(t),i(),window.onresize=i,t.addEventListener(%22click%22,function(e)%7Be.preventDefault(),t.parentNode.removeChild(t),window.onresize=null%7D)%7D%7D();
+```
+2. Click the bookmark to get the viewport size.
+3. Check the top right corner of the browser.
--- viewport-size-checker.js
+++ viewport-size-checker.js
@@ -5,24 +5,36 @@ javascript: (function() {
     link.setAttribute("href", "https://fonts.googleapis.com/css2?family=Barlow:wght@500&display=swap");
     document.head.appendChild(link);
     
-    var container;
-    function viewportSize() {
-        var width, height, size;
-        height = window.innerHeight;
-        width = window.innerWidth;
-        size = width + " X " + height;
-        container.innerHTML = "<span>" + size + "</span>";
+    let viewportSizeContainerClass = "viewport-size-container";
+
+    function createViewportSizeContainer() {
+        let existingContainer = document.getElementsByClassName(viewportSizeContainerClass);
+        if (existingContainer.length > 0) {
+            existingContainer[0].parentNode.removeChild(existingContainer[0]);
+        }
+        
+        let viewportSizeContainer = document.createElement("div");
+        viewportSizeContainer.className = viewportSizeContainerClass;
+        viewportSizeContainer.style.cssText = "position: fixed; padding: 1px 14px; top: 18px; right: 20px; text-align: center; border-radius: 7px; background-color: rgba(255, 106, 69, 0.8); color: #fff; font-family: 'Barlow', 'sans-serif'; font-size: 18px; line-height: 28px; height: 30px; z-index: 10000; font-weight: normal;";
+        document.body.appendChild(viewportSizeContainer);
+        viewportSize(); 
+        window.onresize = viewportSize;
+        viewportSizeContainer.addEventListener('click', function(e) {
+            e.preventDefault();
+            viewportSizeContainer.parentNode.removeChild(viewportSizeContainer);
+            window.onresize = null;
+        });
     }
 
-    container = document.createElement("div");
-    container.style.cssText = "position: fixed; padding: 1px 14px; top: 18px; right: 20px; text-align: center; border-radius: 7px; background-color: rgba(255, 106, 69, 0.8); color: #fff; font-family: 'Barlow', 'sans-serif'; font-size: 18px; line-height: 28px; height: 30px; z-index: 10000; font-weight: normal;";
-    container.innerHTML = "<span>" + window.innerWidth + " X " + window.innerHeight + "</span>";
-    document.body.appendChild(container);
-    window.onresize = viewportSize;
+    function viewportSize() {
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let size = width + " X " + height;
+        let viewportSizeContainers = document.getElementsByClassName(viewportSizeContainerClass);
+        if (viewportSizeContainers.length > 0) {
+            viewportSizeContainers[0].innerHTML = "<span>" + size + "</span>";
+        }
+    }
 
-    container.addEventListener('click', function(e) {
-        e.preventDefault();
-        container.parentNode.removeChild(container);
-        window.onresize = null;
-    })
+    createViewportSizeContainer();
 })();
